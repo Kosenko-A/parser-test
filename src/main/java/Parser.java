42639de0df5d113be6aa2d9b1d6a7f56fc53b263
Kosenko <Kosenko.2000@yandex.ru>
2021-12-05 22:58:15 +0500

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,15 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Parser {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
